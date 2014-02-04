62c6cdbc688f9d83399c89e8aec244701e223804
Adam Schuld <designgumption@gmail.com>
2014-02-03 20:39:37 -0600

    Initial Commit

---
+++ CONFIG.php
@@ -0,0 +1,8 @@
+<?php
+
+//SITE GLOBAL CONFIGURATION
+
+$email = "#"; 							//<-- Your email address
+$subscribe_email = "#"; 					//<<-- Email address for sending you subscribe notification
+$subscribe_notification_subject = "New site subscriber!"; 	//<<-- Email subscribe notification subject
+?>
